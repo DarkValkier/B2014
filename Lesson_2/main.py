7048d869cffa44addce67263e67caafba56d1d9d
DarkValkier <darkvalkier@gmail.com>
2023-03-12 17:58:01 +0200

    Init

---
+++ Lesson_2/main.py
@@ -0,0 +1,15 @@
+from character import Character
+from colorama import Fore
+
+player1 = Character(name='Vasya', health=70, damage=2,
+                    color=Fore.LIGHTRED_EX)
+print(player1)
+
+player2 = Character(name='Petya', health=90, damage=1,
+                    color=Fore.LIGHTBLUE_EX)
+print(player2)
+
+player1.attack(player2)
+
+print(player1)
+print(player2)
